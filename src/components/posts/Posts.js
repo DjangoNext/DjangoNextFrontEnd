ce7ead1e448fbf139c46d82763690f0a0a884a83
Menonro <nitayd2001@gmail.com>
2018-11-19 10:24:27 +0600

    Фиксирую изменения

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -1,4 +1,5 @@
 import React, { PureComponent } from 'react'
+import { Link } from '/../routes'
 
 const Post = () => (
   <div className="col-12 col-lg-6">
@@ -10,6 +11,12 @@ export default class Posts extends PureComponent {
   render () {
     return (
       <article className="container-fluid">
+        <div className="row">
+          <div className="col-auto">{  }</div>
+          <div className="col-auto">
+            <Link href=""><a>{  }</a></Link>
+          </div>
+        </div>
         <div className="row">
           
         </div>
